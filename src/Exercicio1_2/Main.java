098b922e30167c55669de4e8974f65ba69851959
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-06-10 10:16:41 -0300

    Exercicio 1.2 - Verificador de Palíndromos

---
+++ src/Exercicio1_2/Main.java
@@ -0,0 +1,23 @@
+package Exercicio1_2;
+
+import java.util.Scanner;
+
+import static Exercicio1_2.Palindromo.ehPalindromo;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra ou frase: ");
+        String palavra = entrada.nextLine();
+
+        if (ehPalindromo(palavra)) {
+            System.out.println("É um palíndromo!");
+        } else {
+            System.out.println("Não é um palíndromo.");
+        }
+
+        entrada.close();
+    }
+}
+
---
+++ src/Exercicio1_2/Palindromo.java
@@ -0,0 +1,13 @@
+package Exercicio1_2;
+
+public class Palindromo {
+        public static boolean ehPalindromo(String texto) {
+
+            String formatado = texto.replaceAll("\\s+", "").toLowerCase();
+
+            String invertido = new StringBuilder(formatado).reverse().toString();
+
+            return formatado.equals(invertido);
+        }
+
+}
